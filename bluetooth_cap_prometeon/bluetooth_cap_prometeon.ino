10154b6c3beb39d7c185d2f7744e672d1f706a5d
AnaMarcacini <90214423+AnaMarcacini@users.noreply.github.com>
2023-02-17 11:00:44 -0300

    Add files via upload

---
+++ bluetooth_cap_prometeon/bluetooth_cap_prometeon.ino
@@ -0,0 +1,37 @@
+//Carrega a biblioteca SoftwareSerial
+#include <SoftwareSerial.h>
+    
+//Define os pinos para a serial   
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+String command = ""; 
+
+
+void setup()   
+{  
+  //Inicia a serial  
+  Serial.begin(115200);  
+  Serial.println("Digite os comandos AT :");  
+  //Inicia a serial configurada nas portas 10 e 11
+  mySerial.begin(38400);        // se não conectar, utilize 38400
+}  
+
+void loop()  
+{  
+  if (mySerial.available()) 
+  {  
+     while(mySerial.available()) 
+     { command += (char)mySerial.read();  
+     }  
+   Serial.print(command);  
+   command = "";
+  }
+
+  // Read user input if available.  
+  if (Serial.available())
+  {  
+    delay(10); // The DELAY!  
+    mySerial.write(Serial.read());  
+  }  
+}
+
